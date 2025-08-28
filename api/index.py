04957b80dcacb15d563d1b4e101941e1ffcc3e9c
Ryo-pow <shennogliangtai@gmail.com>
2025-08-28 13:54:13 +0900

    Add CORS middleware to allow cross-origin requests

--- api/index.py
+++ api/index.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware # CORSを許可するために追加
 from typing import List
 from vercel_blob import put
 import os
 
 app = FastAPI()
+
+# CORSミドルウェアの設定を追加
+# これにより、どの場所からのアクセスも許可されるようになります
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # すべてのオリジンを許可
+    allow_credentials=True,
+    allow_methods=["*"],  # すべてのHTTPメソッドを許可
+    allow_headers=["*"],  # すべてのヘッダーを許可
+)
 
 @app.get("/")
 def read_root():
