7474be97e885ebc1b003500a76374da6c5ac107b
Ryo-pow <shennogliangtai@gmail.com>
2025-08-28 14:06:20 +0900

    Fix incorrect keyword argument for vercel_blob.put

--- api/index.py
+++ api/index.py
@@ -1,20 +1,20 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
-from fastapi.middleware.cors import CORSMiddleware # CORSを許可するために追加
+from fastapi.middleware.cors import CORSMiddleware
 from typing import List
 from vercel_blob import put
 import os
 
 app = FastAPI()
 
-# CORSミドルウェアの設定を追加
+# CORSミドルウェアの設定
 # これにより、どの場所からのアクセスも許可されるようになります
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # すべてのオリジンを許可
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  # すべてのHTTPメソッドを許可
-    allow_headers=["*"],  # すべてのヘッダーを許可
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
@@ -34,9 +34,9 @@ async def upload_multiple_images(files: List[UploadFile] = File(...)):
         for file in files:
             contents = await file.read()
             
-            # Vercel Blobにファイルをアップロード
+            # Vercel Blobにファイルをアップロード (pathname= を削除)
             blob_result = put(
-                pathname=file.filename, 
+                file.filename, 
                 body=contents, 
                 add_random_suffix=True,
                 access='public'
